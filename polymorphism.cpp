8288af519cde60f3b8953c475995cbd6989c6f24
sunabh07 <sunabhmaheshwari07@gmail.com>
2023-05-22 23:11:22 +0530

    polymorphism in c++

---
+++ polymorphism.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+/*
+polymorphism :- one name and multiple form  
+                eg ; function overloading,operator overloading
+                eg : virtual functions
+
+                compile polymorphism -: compile time me pata chal jata h ki kaunsa function call hoga caused by function and operator overloading
+                run time polymorphism :- program run hone ke baad pata chalta h ki kaunsa function call hoga caused by virtual function.
+
+
+*/
